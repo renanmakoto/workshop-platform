c901d5c27cc5df9a88ed2a4fb81f9f36ac3ee6de
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-02-09 14:36:18 -0300

    Add files via upload

---
+++ src/components/Video.tsx
@@ -0,0 +1,89 @@
+import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react"
+import { DefaultUi, Player, Youtube } from '@vime/react'
+
+import'@vime/core/themes/default.css'
+
+export function Video() {
+    return (
+        <div className="flex-1">
+            <div className="bg-black flex justify-center">
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+                    <Player>
+                        <Youtube videoId="0MIHgif7_Yk" />
+                        <DefaultUi />
+                    </Player>
+                </div>
+            </div>
+            <div className="p-8 max-w-[1100px] mx-auto">
+                <div className="flex items-start gap-16">
+                    <div className="flex-1">
+                        <h1 className="text-2xl font-bold">Class 01 - Opening</h1>
+                        <p className="mt-4 text-gray-200 leading-relaxed">
+                            Today's class we are going to learn about Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officiis repellendus. Eos libero accusamus neque provident. Aperiam ex delectus quos molestias, quod temporibus eius laudantium illum saepe ipsa, a cumque.
+                        </p>
+                        <div className="flex items-center gap-4 mt-6">
+                            <img
+                                className="h-16 w-16 rounded-full border-2 border-blue-500" 
+                                src="https://github.com/renanmakoto.png" 
+                                alt="Teacher's picture"    
+                                
+                            />
+                            <div className="leading-relaxed">
+                                <strong className="font-bold text-2xl block">Renan Makoto</strong>
+                                <span className="text-gray-200 text-sm block">Fullstack Developer @DotExtension</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <a 
+                            href="#" 
+                            className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+                                <DiscordLogo size={ 24 } />
+                            Discord community
+                        </a>
+
+                        <a 
+                            href="#" 
+                            className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+                                <Lightning size={ 24 } />
+                            Access the exercises
+                        </a>
+                    </div>
+                </div>
+                <div className="gap-8 mt-20 grid grid-cols-2">
+                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                        <div className="bg-green-700 h-full p-6 flex items-center">
+                            <FileArrowDown size={ 40 } />
+                        </div>
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">Complementary Content</strong>
+                            <p className="text-sm text-gray-200 mt-2">
+                                Have access to the complementary content in order to perform well.
+                            </p>
+                        </div>
+                        <div className="h-full p-6 flex items-center">
+                            <CaretRight size={ 24 } />
+                        </div>
+                    </a>
+
+                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                        <div className="bg-green-700 h-full p-6 flex items-center">
+                            <FileArrowDown size={ 40 } />
+                        </div>
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">Exclusive Wallpapers</strong>
+                            <p className="text-sm text-gray-200 mt-2">
+                                Download the wallpapers to customize your computer!
+                            </p>
+                        </div>
+                        <div className="h-full p-6 flex items-center">
+                            <CaretRight size={ 24 } />
+                        </div>
+                    </a>
+
+                    <a href=""></a>
+                </div>
+            </div>
+        </div>
+    ) 
+}
